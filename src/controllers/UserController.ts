e5b4921ce24b724d6a3140cab23e585028b61565
Rafael.brito <rafael.brito@grancursosonline.com.br>
2021-03-19 14:54:22 -0300

    feat: Cria entidades de  create de surveys. feat: Adiciona repositorys de usuario e surveys.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,11 @@
 import {Request, Response} from "express";
-import {getRepository} from "typeorm";
-import {User} from "../models/User";
+import {getCustomRepository} from "typeorm";
+import {UserRepository} from "../repositories/UserRepository";
 
 class UserController {
  async create(request: Request, response: Response) {
     const {name, email} = request.body;
-    const userRepository = getRepository(User);
+    const userRepository = getCustomRepository(UserRepository);
     const userAlreadExists = await userRepository.findOne({
         email
     });
