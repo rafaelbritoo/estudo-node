a2a1b2274e88bd07a699bd038ffa1b430fec60a0
Rafael.brito <rafael.brito@grancursosonline.com.br>
2021-03-19 13:36:52 -0300

    feat: Valida se usuario ja esta cadastrado.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -6,6 +6,15 @@ class UserController {
  async create(request: Request, response: Response) {
     const {name, email} = request.body;
     const userRepository = getRepository(User);
+    const userAlreadExists = await userRepository.findOne({
+        email
+    });
+    if (userAlreadExists) {
+        return response.status(400).json({
+           error: "Usuario já cadastrado na base de dados!"
+        });
+    }
+
     const user = userRepository.create({
         name, email
     });
